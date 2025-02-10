0a9785efdb6f8008b895905655426ecc6a77891e
dh <dohyun134@naver.com>
2025-02-10 16:42:18 +0900

    signup

--- shopping-web/src/App.jsx
+++ shopping-web/src/App.jsx
@@ -1,14 +1,17 @@
-import './App.css'
-import { Header } from './components/Header'
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import { Header } from "./components/Header";
+import SignUp from "./components/signup/SignUp";
 
 function App() {
-  
-
   return (
-    <>
+    <Router>
       <Header />
-    </>
-  )
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- shopping-web/src/components/Header.jsx
+++ shopping-web/src/components/Header.jsx
@@ -1,29 +1,36 @@
+import { Link } from "react-router-dom";
 import "../components/Header.css";
 import Search from "./Search";
+import "../components/signup/SignUp";
 
 export const Header = () => {
   return (
     <header className="header-wrapper">
-      {/* 로고 및 네비게이션 */}
       <div className="frame-2">
         <img src="/Logo.png" alt="Logo" className="logo" />
 
         <nav className="frame-3">
-          <a href="/" className="nav-link">Home</a>
-          <a href="/contact" className="nav-link">Contact</a>
-          <a href="/about" className="nav-link">About</a>
-          <a href="/signup" className="nav-link">Sign Up</a>
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/contact" className="nav-link">
+            Contact
+          </Link>
+          <Link to="/about" className="nav-link">
+            About
+          </Link>
+          <Link to="/SignUp" className="nav-link">
+            Sign Up
+          </Link>
         </nav>
       </div>
 
-        {/* 검색 */}
-        <Search />
-        <div className="frame-5">
-          <a href="/wishlist" className="icon wishlist"></a>
-          <a href="/cart" className="icon cart"></a>
-          <a href="/user" className="icon user"></a>
-        </div>
-     
+      <Search />
+      <div className="frame-5">
+        <Link to="/wishlist" className="icon wishlist"></Link>
+        <Link to="/cart" className="icon cart"></Link>
+        <Link to="/user" className="icon user"></Link>
+      </div>
     </header>
   );
 };
