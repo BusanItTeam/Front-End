c9acfd7eceaf43bf170ee7a531d9c0be8ad8594a
EJ <harrygina09@naver.com>
2025-02-11 12:56:59 +0900

    sidebar

--- shopping-web/src/App.jsx
+++ shopping-web/src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
 import Login from "./components/Login";
 import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <Router>
       <Header />
+      <Sidebar />
       <Routes>
         <Route path="/login" element={<Login />} />
       </Routes>
--- shopping-web/src/components/Footer.jsx
+++ shopping-web/src/components/Footer.jsx
@@ -3,104 +3,106 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div class="footer">
-      <div class="frame">
-        <div class="under-line"></div>
-        <div class="frame-wrapper">
-          <div class="div">
-            <img class="icon-copyright" src="/copyright.png" />
-            <p class="text-wrapper">Copyright Rimel 2025. All right reserved</p>
+    <div className="footer">
+      <div className="frame">
+        <div className="under-line"></div>
+        <div className="frame-wrapper">
+          <div className="div">
+            <img className="icon-copyright" src="/copyright.png" />
+            <p className="text-wrapper">
+              Copyright Rimel 2025. All right reserved
+            </p>
           </div>
         </div>
       </div>
 
-      <div class="frame-2">
-        <div class="frame-3">
-          <div class="frame-4">
-            <div class="frame-4">
-              <div class="logo">
-                <div class="text-wrapper-2">Exclusive</div>
+      <div className="frame-2">
+        <div className="frame-3">
+          <div className="frame-4">
+            <div className="frame-4">
+              <div className="logo">
+                <div className="text-wrapper-2">Exclusive</div>
               </div>
-              <div class="text-wrapper-3">Subscribe</div>
+              <div className="text-wrapper-3">Subscribe</div>
             </div>
-            <p class="text-wrapper-4">Get 10% off your first order</p>
+            <p className="text-wrapper-4">Get 10% off your first order</p>
           </div>
-          <div class="send-mail">
-            <div class="text-wrapper-5">Enter your email</div>
-            <img class="img" src="/send.png" />
+          <div className="send-mail">
+            <div className="text-wrapper-5">Enter your email</div>
+            <img className="img" src="/send.png" />
           </div>
         </div>
 
-        <div class="frame-4">
-          <div class="text-wrapper-6">Support</div>
-          <div class="frame-3">
-            <p class="element-bijoy-sarani">
+        <div className="frame-4">
+          <div className="text-wrapper-6">Support</div>
+          <div className="frame-3">
+            <p className="element-bijoy-sarani">
               111 Bijoy sarani, Dhaka,&nbsp;&nbsp;DH 1515, Korea.
             </p>
-            <div class="text-wrapper-4">exclusive@gmail.com</div>
-            <div class="text-wrapper-4">+88015-88888-9999</div>
+            <div className="text-wrapper-4">exclusive@gmail.com</div>
+            <div className="text-wrapper-4">+88015-88888-9999</div>
           </div>
         </div>
 
-        <div class="frame-4">
-          <div class="text-wrapper-6">Account</div>
-          <div class="frame-3">
-            <a href="/mypage" class="text-wrapper-7">
+        <div className="frame-4">
+          <div className="text-wrapper-6">Account</div>
+          <div className="frame-3">
+            <a href="/mypage" className="text-wrapper-7">
               My Page
             </a>
-            <a href="/login" class="text-wrapper-4">
+            <a href="/login" className="text-wrapper-4">
               Login
             </a>
-            <a href="/signup" class="text-wrapper-4">
+            <a href="/signup" className="text-wrapper-4">
               Sign Up
             </a>
-            <a href="/cart" class="text-wrapper-4">
+            <a href="/cart" className="text-wrapper-4">
               Cart
             </a>
-            <a href="/wishlist" class="text-wrapper-4">
+            <a href="/wishlist" className="text-wrapper-4">
               Wishlist
             </a>
           </div>
         </div>
 
-        <div class="frame-4">
-          <div class="text-wrapper-6">Quick Link</div>
-          <div class="frame-3">
-            <div class="text-wrapper-7">Privacy Policy</div>
-            <div class="text-wrapper-4">Terms Of Use</div>
-            <a href="/faq" class="text-wrapper-4">
+        <div className="frame-4">
+          <div className="text-wrapper-6">Quick Link</div>
+          <div className="frame-3">
+            <div className="text-wrapper-7">Privacy Policy</div>
+            <div className="text-wrapper-4">Terms Of Use</div>
+            <a href="/faq" className="text-wrapper-4">
               FAQ
             </a>
-            <a href="/contact" class="text-wrapper-4">
+            <a href="/contact" className="text-wrapper-4">
               Contact
             </a>
           </div>
         </div>
 
-        <div class="frame-4">
-          <div class="frame-4">
-            <div class="text-wrapper-6">Download App</div>
-            <div class="frame-5">
-              <p class="p">Save $3 with App New User Only</p>
-              <div class="frame-6">
-                <div class="qr-code">
-                  <img class="qrcode" src="/QRcode.png" />
+        <div className="frame-4">
+          <div className="frame-4">
+            <div className="text-wrapper-6">Download App</div>
+            <div className="frame-5">
+              <p className="p">Save $3 with App New User Only</p>
+              <div className="frame-6">
+                <div className="qr-code">
+                  <img className="qrcode" src="/QRcode.png" />
                 </div>
-                <div class="frame-7">
-                  <img class="google-play" src="/google.png" />
-                  <div class="app-store">
-                    <img class="download-appstore" src="/appstore.png" />
+                <div className="frame-7">
+                  <img className="google-play" src="/google.png" />
+                  <div className="app-store">
+                    <img className="download-appstore" src="/appstore.png" />
                   </div>
                 </div>
               </div>
             </div>
           </div>
 
-          <div class="frame-8">
-            <img class="img" src="/facebook.png" />
-            <img class="img" src="/twitter.png" />
-            <img class="img" src="/instagram.png" />
-            <img class="img" src="/linkedin.png" />
+          <div className="frame-8">
+            <img className="img" src="/facebook.png" />
+            <img className="img" src="/twitter.png" />
+            <img className="img" src="/instagram.png" />
+            <img className="img" src="/linkedin.png" />
           </div>
         </div>
       </div>
