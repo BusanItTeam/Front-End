18d39bd2b4a0b7dc7eac60ffaf8d78059e451bc5
[JUNG RYUN KIM] <[kim665700@gmail.com]>
2025-02-13 11:31:23 +0900

    아아앙아아아앙

--- shopping-web/src/components/login/Login.jsx
+++ shopping-web/src/components/login/Login.jsx
@@ -59,7 +59,7 @@ const Login = () => {
   return (
     <div className="flex h-screen bg-white mt-30 mb-30">
       {/* 왼쪽 이미지 영역 */}
-      <div className="hidden md:flex w-[60%] min-h-[700px] bg-[#cbe4e8] overflow-hidden relative rounded-tr-md rounded-br-md">
+      <div className="hidden md:flex w-[61%] min-h-[700px] bg-[#cbe4e8] overflow-hidden relative rounded-tr-md rounded-br-md">
         <img
           src="/dl.beatsnoop.png"
           alt="Shopping Cart"
