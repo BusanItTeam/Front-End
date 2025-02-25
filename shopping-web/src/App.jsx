ed137c495c0482d82a67c9b7f45d23bcf9885fb2
dh <dohyun134@naver.com>
2025-02-25 10:11:42 +0900

    dd

--- shopping-web/src/App.jsx
+++ shopping-web/src/App.jsx
@@ -43,7 +43,6 @@ function App() {
     <ContextProvider>
       <Router>
         <Header />
-
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/productcategory" element={<ProductCategory />} />
@@ -62,11 +61,6 @@ function App() {
           <Route path="/orderpage" element={<OrderPage />} />
           <Route path="/orderpage/payment" element={<Payment />} />
           <Route path="/orderpage/ordercomplete" element={<OrderComplete />} />
-
-
-          {/* 관리자 페이지 라우트 */}
-
-
           <Route
             path="/admin"
             element={
